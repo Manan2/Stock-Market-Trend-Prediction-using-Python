4cda5a8e7e31fb41691414868197c8ad859e92ec
Manan Thanawala <thanat60@gmail.com>
2019-03-15 17:53:32 -0500

    Add files via upload

---
+++ rnnappl.py
@@ -0,0 +1,68 @@
+# Recurrent Neural Network
+
+# Part 1 - Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the training set
+training_set = pd.read_csv('trainaapl.csv')
+training_set = training_set.iloc[:,1:2].values
+
+# Feature Scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler()
+training_set = sc.fit_transform(training_set)
+
+# Getting the inputs and the ouputs
+X_train = training_set[0:1004]
+y_train = training_set[1:1005]
+
+# Reshaping
+X_train = np.reshape(X_train, (1004, 1, 1))
+
+# Part 2 - Building the RNN
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+
+# Initialising the RNN
+regressor = Sequential()
+
+# Adding the input layer and the LSTM layer
+regressor.add(LSTM(units = 4, activation = 'sigmoid', input_shape = (None, 1)))
+
+# Adding the output layer
+regressor.add(Dense(units = 1))
+
+# Compiling the RNN
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+# Fitting the RNN to the Training set
+regressor.fit(X_train, y_train, batch_size = 32, epochs = 200)
+
+# Part 3 - Making the predictions and visualising the results
+
+# Getting the real stock price of 2017
+test_set = pd.read_csv('testaapl.csv')
+real_stock_price = test_set.iloc[:,1:2].values
+
+# Getting the predicted stock price of 2017
+inputs = real_stock_price
+inputs = sc.transform(inputs)
+inputs = np.reshape(inputs, (251, 1, 1))
+predicted_stock_price = regressor.predict(inputs)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
+# Visualising the results
+plt.plot(real_stock_price, color = 'red', label = 'Real Apple Stock Price')
+plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted Apple Stock Price')
+plt.title('Apple Stock Prediction')
+plt.xlabel('Time')
+plt.ylabel('Apple stock price')
+plt.legend()
+plt.show()
